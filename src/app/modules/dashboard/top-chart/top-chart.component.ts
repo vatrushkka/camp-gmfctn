37263f1fa05691b0a8b9717be047eebe007926c3
vatrushkka <valiakoziyra@gmail.com>
2021-04-04 15:38:47 +0300

    top-chart

--- src/app/modules/dashboard/top-chart/top-chart.component.ts
+++ src/app/modules/dashboard/top-chart/top-chart.component.ts
@@ -7,17 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopChartComponent implements OnInit {
 
+  users = [
+    {icon: '../assets/dashboard-img/cat.jpg', firstName:'Zapam', lastName: 'Tskutka', xp:'47', color:'#32a852'},
+    {icon: '../assets/dashboard-img/cat-avatar.jpeg', firstName:'Vasya', lastName: 'Petrov', xp:'386', color:'#3265a8'},
+    {icon: '../assets/dashboard-img/avatar.jpg', firstName:'Vatrushka', lastName: 'Vart', xp:'137', color:'#a8324c'},
+    {icon: '../assets/dashboard-img/cat-cat.jpg', firstName:'Valia', lastName: 'Val', xp:'39', color:'#8ca832'},
+    {icon: '../assets/dashboard-img/cat-cat.jpg', firstName:'Apple', lastName: 'Corporation', xp:'248', color:'#a83232'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  users = [
-    {icon: '../assets/dashboard-img/cat.jpg', firstName:'Zapam', lastName: 'Tskutka', xp:'47', color:'#32a852'},
-    {icon: '../assets/dashboard-img/cat-avatar.jpg', firstName:'Vasya', lastName: 'Petrov', xp:'386', color:'#3265a8'},
-    {icon: '', firstName:'Vatrushka', lastName: 'Vart', xp:'137', color:'#a8324c'},
-    {icon: '', firstName:'Valia', lastName: 'Val', xp:'39', color:'#8ca832'},
-    {icon: '', firstName:'Apple', lastName: 'Corporation', xp:'248', color:'#a83232'}
-  ]
   
 }
