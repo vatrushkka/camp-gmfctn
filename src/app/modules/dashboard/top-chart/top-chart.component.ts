dfdc0dabdb8dae63fb5d35abe20afa23f12fa36f
vatrushkka <valiakoziyra@gmail.com>
2021-04-04 14:23:04 +0300

    top-chart start

--- src/app/modules/dashboard/top-chart/top-chart.component.ts
+++ src/app/modules/dashboard/top-chart/top-chart.component.ts
@@ -12,4 +12,12 @@ export class TopChartComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  users = [
+    {icon: '../assets/dashboard-img/cat.jpg', firstName:'Zapam', lastName: 'Tskutka', xp:'47', color:'#32a852'},
+    {icon: '../assets/dashboard-img/cat-avatar.jpg', firstName:'Vasya', lastName: 'Petrov', xp:'386', color:'#3265a8'},
+    {icon: '', firstName:'Vatrushka', lastName: 'Vart', xp:'137', color:'#a8324c'},
+    {icon: '', firstName:'Valia', lastName: 'Val', xp:'39', color:'#8ca832'},
+    {icon: '', firstName:'Apple', lastName: 'Corporation', xp:'248', color:'#a83232'}
+  ]
+  
 }
