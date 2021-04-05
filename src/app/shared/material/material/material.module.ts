a1910136cdbbf71b2d882b116c5c5b5c6d065f5e
vatrushkka <valiakoziyra@gmail.com>
2021-04-05 13:39:49 +0300

    smth

--- src/app/shared/material/material/material.module.ts
+++ src/app/shared/material/material/material.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTabsModule} from '@angular/material/tabs'
+
+import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
-import {MatSidenavModule} from '@angular/material/sidenav'
-import {MatMenuModule} from '@angular/material/menu'
-import {MatGridListModule} from '@angular/material/grid-list'
-import {MatListModule} from '@angular/material/list'
-import {MatTableModule} from '@angular/material/table'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatStepperModule} from '@angular/material/stepper';
 
 
 @NgModule({
   declarations: [  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatListModule,
+    MatTableModule,
+    MatStepperModule
   ],
   exports: [
     MatTabsModule,
@@ -29,7 +43,8 @@ import {MatTableModule} from '@angular/material/table'
     MatMenuModule,
     MatGridListModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatStepperModule
     ]
 })
 export class MaterialModule { }
