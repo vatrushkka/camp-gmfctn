c9fa273279431cb879d8896d0d683d95f2e7d879
vatrushkka <valiakoziyra@gmail.com>
2021-04-07 00:31:35 +0300

    styling navigation

--- src/app/+dashboard/data/challengesData.ts
+++ src/app/+dashboard/data/challengesData.ts
@@ -35,9 +35,5 @@ export const endedChallenges = [
   {
     img: '../assets/img/dashboard-img/catt.jpg',
     chName: '37 Cats'
-  },
-  {
-    img: '../assets/img/dashboard-img/catt.jpg',
-    chName: '37 Cats'
   }
 ];
