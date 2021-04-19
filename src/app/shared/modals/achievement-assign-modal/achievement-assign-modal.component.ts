73055d751eee7344a4000a0b839670aa3042b339
vatrushkka <valiakoziyra@gmail.com>
2021-04-19 15:30:35 +0300

    form changed

--- src/app/shared/modals/achievement-assign-modal/achievement-assign-modal.component.ts
+++ src/app/shared/modals/achievement-assign-modal/achievement-assign-modal.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { achievements } from 'src/app/modules/+dashboard/data/personal-achievements-data';
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-achievement-assign-modal',
   templateUrl: './achievement-assign-modal.component.html',
   styleUrls: ['./achievement-assign-modal.component.scss']
 })
-export class AchievementAssignModalComponent { }
+export class AchievementAssignModalComponent implements OnInit {
+  public achievements = achievements;
+
+  inputForm = new FormGroup({
+    achievement: new FormControl(''),
+    message: new FormControl('')
+  });
+
+  constructor(private readonly fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.inputForm = new FormGroup({
+      achievement: this.fb.control('', Validators.required),
+      message: this.fb.control('', Validators.required)
+    });
+  }
+
+  submit(): void {
+    if (this.inputForm.valid) {
+      alert('You requested an achievement');
+    }
+  }
+}
--- src/app/shared/modals/achievement-assign-modal/achievement-assign-modal.module.ts
+++ src/app/shared/modals/achievement-assign-modal/achievement-assign-modal.module.ts
@@ -2,23 +2,19 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgSelectModule } from '@ng-select/ng-select';
 
 // components
 import {AchievementAssignModalComponent} from './achievement-assign-modal.component';
-import {AchievementAssignFormComponent} from './achievement-assign-form/achievement-assign-form.component';
 import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
-    AchievementAssignModalComponent,
-    AchievementAssignFormComponent
+    AchievementAssignModalComponent
   ],
   imports: [
     MaterialModule,
     ReactiveFormsModule,
-    CommonModule,
-    NgSelectModule
+    CommonModule
   ]
 })
 export class AchievementAssignModalModule { }
