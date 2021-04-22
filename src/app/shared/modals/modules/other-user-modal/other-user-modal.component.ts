ed7aab7165b013727ca2109b8ee2f77b68514da5
vatrushkka <valiakoziyra@gmail.com>
2021-04-22 19:49:32 +0300

    other user modal done

--- src/app/shared/modals/modules/other-user-modal/other-user-modal.component.ts
+++ src/app/shared/modals/modules/other-user-modal/other-user-modal.component.ts
@@ -9,6 +9,44 @@ import { ThanksModalComponent } from '../thanks-modal/thanks-modal.component';
   styleUrls: ['./other-user-modal.component.scss']
 })
 export class OtherUserModalComponent {
+  achievements = [
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Turbo Power',
+      amount: 1
+    },
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Turbo Power',
+      amount: 2
+    },
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Power',
+      amount: 2
+    },
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Power',
+      amount: 1
+    },
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Party Power',
+      amount: 1
+    },
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Turbo Power',
+      amount: 3
+    },
+    {
+      img: '../assets/img/dashboard-img/badge.png',
+      title: 'Exoft Turbo Power',
+      amount: 2
+    }
+  ];
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: User,
     private dialog: MatDialog) { }
